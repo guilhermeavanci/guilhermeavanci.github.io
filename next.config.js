9701b8c64aad15d3aa8dbe0a24be8482db8b9ca3
Guilherme Pires <guilhermeavancip@gmail.com>
2023-03-31 18:55:32 -0300

    Adding assetPrefix and nojekyll file

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
+const isProd = process.env.NODE_ENV === 'production'
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  assetPrefix: isProd ? '/guilhermeavanci.github.io/' : '',
   experimental: {
     appDir: true,
   },
